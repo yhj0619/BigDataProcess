0da587e963c50bca7bc947fcfab1a149ee20c63b
양혜지 <yhj061987@gmail.com>
2022-10-15 02:50:06 +0900

    add hw2 code

--- HW2/student20200984.py
+++ HW2/student20200984.py
@@ -9,7 +9,6 @@ ws = wb['Sheet1']
 row_id = 1
 total_list = []
 total_sort = []
-str_grade = ['A+','A0','B+','B0','C+','C0']
 
 for row in ws:
 	if row_id != 1:
@@ -23,19 +22,25 @@ for row in ws:
 
 total_sort= sorted(total_list,reverse=True)
 
-student = 0
-
-row_id_new = 1
+row_new = 1
 for row in ws:
-	if row_id_new != 1:
-		if len(total_sort) * 0.3:
-			ws.cell(row=row_id_new, column=8).value = 'A0'
-	row_id_new += 1	
- 
+	if row_new != 1:
+		for n in total_sort:
+			A_cnt = int(len(total_sort) * 0.3)
+			B_cnt = int(len(total_sort)*0.7) - A_cnt
 		
+			for i in range(A_cnt):
+				if ws.cell(row=row_new, column=7).value == total_sort[i]:
+					ws.cell(row=row_new, column=8).value = 'A0'	
+			
+			for i in range(A_cnt,A_cnt+ B_cnt):
+				if ws.cell(row=row_new, column=7).value == total_sort[i]:
+					ws.cell(row=row_new,column=8).value ='B0'
 
+			for i in range(A_cnt+B_cnt,len(total_sort)):
+				if ws.cell(row=row_new, column=7).value == total_sort[i]:
+					ws.cell(row=row_new,column=8).value = 'C0'
 
-
-
+	row_new += 1
 
 wb.save( "student.xlsx" )
