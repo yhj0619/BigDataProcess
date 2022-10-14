e249abd517f9de8c52b104aee567a75aef2c31a1
양혜지 <yhj061987@gmail.com>
2022-10-15 00:37:18 +0900

    add hw2 code

--- HW2/student20200984.py
+++ HW2/student20200984.py
@@ -2,15 +2,40 @@
 
 import openpyxl
 
-wb = openpyxl.load_workbook( "20200984.xlsx" )
+wb = openpyxl.load_workbook( "student.xlsx" )
 ws = wb['Sheet1']
 
-row_id = 1;
+
+row_id = 1
+total_list = []
+total_sort = []
+str_grade = ['A+','A0','B+','B0','C+','C0']
+
 for row in ws:
 	if row_id != 1:
-		sum_v = ws.cell(row = row_id, column = 3).value *0.3
-	row_od += 1
+		sum_v = ws.cell(row = row_id, column = 3).value * 0.3
+		sum_v += ws.cell(row = row_id, column = 4).value * 0.35
+		sum_v += ws.cell(row = row_id, column = 5).value * 0.34
+		sum_v += ws.cell(row = row_id, column = 6).value
+		ws.cell(row=row_id, column = 7).value = sum_v
+		total_list.append(sum_v)
+	row_id += 1
 
-wb.save( "20200984.xlsx" )
+total_sort= sorted(total_list,reverse=True)
+
+student = 0
+
+row_id_new = 1
+for row in ws:
+	if row_id_new != 1:
+		if len(total_sort) * 0.3:
+			ws.cell(row=row_id_new, column=8).value = 'A0'
+	row_id_new += 1	
+ 
+		
 
 
+
+
+
+wb.save( "student.xlsx" )
