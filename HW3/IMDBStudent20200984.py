2f172c01d0567518d1860553cd37370fb6a2a028
양혜지 <yhj061987@gmail.com>
2022-10-30 19:25:49 +0900

    add HW3 code

--- HW3/IMDBStudent20200984.py
+++ HW3/IMDBStudent20200984.py
@@ -3,6 +3,9 @@ import sys
 
 with open(sys.argv[1], "rt") as f:
 	genre_dict = {}
+	dict_no_dup = dict()
+	result_dict = {}
+
 	data = f.read()
 	lines = data.split("\n")
 	with open(sys.argv[2], "wt") as f1:
@@ -15,4 +18,9 @@ with open(sys.argv[1], "rt") as f:
 					genre_dict[genre_title] = 1
 				else:
 					genre_dict[genre_title] += 1
-			f1.write('{} {}\n'.format(genre_title, genre_dict[genre_title]))
+			for key, val in genre_dict.items():
+				dict_no_dup[val] = key
+			for key, val in dict_no_dup.items():
+				result_dict[val] = key
+		for k, v in result_dict.items():
+			f1.write('{} {}\n'.format(k,v))
