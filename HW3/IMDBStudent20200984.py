633ba5fd571fcdbab5c7c066c3fc16d513c60439
양혜지 <yhj061987@gmail.com>
2022-10-31 12:23:28 +0900

    add HW3 code

--- HW3/IMDBStudent20200984.py
+++ HW3/IMDBStudent20200984.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
+
 import sys
 
 with open(sys.argv[1], "rt") as f:
 	genre_dict = {}
 	dict_no_dup = dict()
 	result_dict = {}
-
+	
 	data = f.read()
 	lines = data.split("\n")
-	with open(sys.argv[2], "wt") as f1:
+	with open(sys.argv[2],"wt") as f1:
 		for line in lines:
 			movie = line.split("::")
 			genres = movie[2]
 			genre = genres.split("|")
 			for genre_title in genre:
-				if(genre_title in genre_dict) == False:
+				if genre_title not in genre_dict:
 					genre_dict[genre_title] = 1
 				else:
 					genre_dict[genre_title] += 1
