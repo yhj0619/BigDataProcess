fa3d8af05a68ce85d679fd94ef55b9c4d7b3d389
양혜지 <yhj061987@gmail.com>
2022-11-01 23:54:18 +0900

    add HW3 code

--- HW3/UBERStudent20200984.py
+++ HW3/UBERStudent20200984.py
@@ -5,6 +5,8 @@ import datetime
 
 with open(sys.argv[1], "rt") as f:
 	uber_dict = {}
+	v_sum = 0
+	t_sum = 0
 	days=['MON','TUE','WED','THU','FRI','SAT','SUN']
 	data = f.read()
 	lines = data.split("\n")
@@ -13,14 +15,21 @@ with open(sys.argv[1], "rt") as f:
 			text = line.split(",")
 			region = text[0]
 			day = text[1]
-			vehicles = text[2]
-			trips = text[3]
+			vehicles = int(text[2])
+			trips = int(text[3])
 			date = day.split("/")
 			m = int(date[0])
 			d = int(date[1])
 			y = int(date[2])
 			str_day = days[datetime.date(y,m,d).weekday()]
 			new_key = region + "," + str_day
-			new_value = vehicles + "," + trips
-			f1.write('%s %s\n' % (new_key, new_value))
-
+			if new_key not in uber_dict:
+				new_value = str(vehicles) + "," + str(trips)
+				uber_dict[new_key] = new_value
+			else:
+				v_sum += vehicles
+				t_sum += trips
+				new_value = str(v_sum) + "," + str(trips)
+				uber_dict[new_key] = new_value
+		for i in uber_dict:
+			f1.write('%s %s\n' % (i, uber_dict[i]))
