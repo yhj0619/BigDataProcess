ef7fc15d581f07a4883efc2529a1a9af62a4379f
양혜지 <yhj061987@gmail.com>
2022-11-02 03:42:42 +0900

    add hw3 code

--- HW3/UBERStudent20200984.py
+++ HW3/UBERStudent20200984.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 
 import sys
-import datetime
+import calendar
 
 with open(sys.argv[1], "rt") as f:
 	uber_dict = dict()
-	v_sum = 0
-	t_sum = 0
 	days=['MON','TUE','WED','THU','FRI','SAT','SUN']
 	data = f.read()
 	lines = data.split("\n")
+
 	for line in lines:
 		text = line.split(",")
 		region = text[0]
 		day = text[1]
 		vehicles = text[2]
 		trips = text[3]
+
 		date = day.split("/")
 		m = int(date[0])
 		d = int(date[1])
 		y = int(date[2])
-		str_day = days[datetime.date(y,m,d).weekday()]
-		key = region + "," + str_day
+		eng_day = calendar.weekday(y,m,d)
+
+		key = region + "," + days[eng_day]
+
 		if key not in uber_dict:
 			uber_dict[key] = vehicles + "," + trips
 		else:
