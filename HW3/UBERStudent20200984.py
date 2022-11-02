1da304c294dd11cab86c2a045dcdd9f6441edfa9
양혜지 <yhj061987@gmail.com>
2022-11-02 09:00:38 +0900

    add hw3 code

--- HW3/UBERStudent20200984.py
+++ HW3/UBERStudent20200984.py
@@ -6,30 +6,21 @@ import calendar
 with open(sys.argv[1], "rt") as f:
 	uber_dict = dict()
 	days=['MON','TUE','WED','THU','FRI','SAT','SUN']
-	data = f.read()
-	lines = data.split("\n")
 
-	for line in lines:
+	for line in f:
 		text = line.split(",")
-		region = text[0]
-		day = text[1]
-		vehicles = text[2]
-		trips = text[3]
 
-		date = day.split("/")
-		m = int(date[0])
-		d = int(date[1])
-		y = int(date[2])
-		eng_day = calendar.weekday(y,m,d)
+		date = text[1].split("/")
+		eng_day = calendar.weekday(int(date[2]), int(date[0]), int(date[1]))
 
-		key = region + "," + days[eng_day]
+		key = text[0] + "," + days[eng_day]
 
 		if key not in uber_dict:
-			uber_dict[key] = vehicles + "," + trips
+			uber_dict[key] = text[2] + "," + text[3]
 		else:
 			new_list = uber_dict[key].split(",")
-			v_sum = int(new_list[0]) + int(vehicles)
-			t_sum = int(new_list[1]) + int(trips)
+			v_sum = int(new_list[0]) + int(text[2])
+			t_sum = int(new_list[1]) + int(text[3])
 			uber_dict[key] = str(v_sum) + "," +str(t_sum)
 
 with open(sys.argv[2], "wt") as f:
